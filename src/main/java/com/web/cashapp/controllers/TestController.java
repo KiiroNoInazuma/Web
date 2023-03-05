99ac7e38a5f4f08aee34527f1ac80a6c603c51bb
John Rey <adioskid@mail.ru>
2023-03-05 14:57:20 +0300

    WebDev

---
+++ src/main/java/com/web/cashapp/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.web.cashapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/ha")
+    public String test(@RequestParam int x) {
+        return "TEST!!! " + x;
+    }
+}
