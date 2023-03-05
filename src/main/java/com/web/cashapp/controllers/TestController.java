cfe06bb550ab7e099d3d02c0e8b23034e8cfe329
John Rey <adioskid@mail.ru>
2023-03-05 15:11:20 +0300

    WebDev

--- src/main/java/com/web/cashapp/controllers/TestController.java
+++ src/main/java/com/web/cashapp/controllers/TestController.java
@@ -1,13 +1,21 @@
 package com.web.cashapp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/ha")
-    public String test(@RequestParam String x) {
-        return "TEST!!! " + x;
+    @GetMapping("/")
+    public String test() {
+        return "Приложение запущено";
+    }
+    @GetMapping("/info")
+    public String info() {
+        String myName = "John Rey; ";
+        String nameProject = "CashApp; ";
+        String date = "05.03.2023 15:06; ";
+        String description = "Мой проект;";
+
+        return myName+nameProject+date+description;
     }
 }
